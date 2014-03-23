d84653cac08e12a514e29c5ef8a2a467cf6930bb
CaptainMustang8 <nmorris@smu.edu>
2014-03-23 18:27:00 -0500

    Added header for searchterms.php

---
+++ searchterms.php
@@ -0,0 +1,15 @@
+<!--searchterms.php-->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link href="css/styles_home.css" rel="stylesheet" />
+	<link href="css/styles.css" rel="stylesheet" />
+	<title>UPresent -- Contact Us</title>
+</head>
+<body>
+
+
+</body>
+
+</html>
