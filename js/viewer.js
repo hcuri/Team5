00374a3a7bc9ca5eceaad100687371596950adcf
jkpolitz <jpolitz@smu.edu>
2014-03-25 18:56:33 -0500

    styling and formating updates to viewer

--- js/viewer.js
+++ js/viewer.js
@@ -2,4 +2,5 @@
 $(document).ready(function() {
 	$( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
     $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+	
 });
