840887734f66753f3684b340d559900f32737893
ntaylorbishop <ntaylorbishop@gmail.com>
2014-03-24 23:04:24 -0500

    Registration works with the two new input fields

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@ require '../php/lib.php';
 $app = new Slim();
 
 $app->get('/verify/:username/:pass', 'verifyRegistered');
-$app->get('/registered/:email', 'checkIfRegistered');
+$app->get('/registered/:email/:username', 'checkIfRegistered');
 $app->post('/register', 'registerUser');
 
 $app->run();
@@ -31,18 +31,30 @@ function verifyRegistered($username, $password) {
 	}
 }
 
-function checkIfRegistered($email) {
-	$sql = "SELECT * FROM Users WHERE email=:email";
+function checkIfRegistered($email, $username) {
+	$sqlEmail = "SELECT * FROM Users WHERE email=:email";
+	$sqlUsername = "SELECT * FROM Users WHERE username=:username";
 
 	try {
 		$db = dbconnect();
-		$stmt = $db->prepare($sql);  
+
+		// Check email
+		$stmt = $db->prepare($sqlEmail);  
 		$stmt->bindParam("email", $email);
 		$stmt->execute();  
 		if($stmt->rowCount() > 0)
-			echo '{"email_registered": true}';  
+			echo '{"email": true,';  
 		else 
-			echo '{"email_registered": false}';
+			echo '{"email": false,';
+
+		// Check username
+		$stmt = $db->prepare($sqlUsername);  
+		$stmt->bindParam("username", $username);
+		$stmt->execute();  
+		if($stmt->rowCount() > 0)
+			echo '"username": true}';  
+		else 
+			echo '"username": false}';
 		$db = null;
 	} catch(PDOException $e) {
 		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
@@ -54,13 +66,15 @@ function registerUser() {
 	$request = Slim::getInstance()->request();
 	$user = json_decode($request->getBody());
 	$pass = password_hash($user->pass, PASSWORD_DEFAULT);
-	$sql = "INSERT INTO Users VALUES (DEFAULT, 'NONE', 'NONE', :username, :email, :pass, 'NONE')";
+	$sql = "INSERT INTO Users VALUES (DEFAULT, :fName, :lName, :username, :email, :pass, 'NONE')";
 	try {
 		$db = dbconnect();
 		$stmt = $db->prepare($sql);   
+		$stmt->bindParam("fName", $user->fName);
+		$stmt->bindParam("lName", $user->lName);
 		$stmt->bindParam("username", $user->username);
-		$stmt->bindParam("pass", $pass);
 		$stmt->bindParam("email", $user->email);
+		$stmt->bindParam("pass", $user->pass);
 	       	$stmt->execute();
 		$db = null; 
 		echo json_encode($user); 
--- js/main.js
+++ js/main.js
@@ -17,13 +17,12 @@ function checkLogin(lForm) {
 	return check;
 }
 
-
-
 function checkRegister(pform) {
 	var username = pform.signUpUsername.value;
 	var email = pform.signUpEmail.value;
 	var pwd = pform.signUpPassword.value;
 	var pwdC = pform.signUpPasswordC.value
+	
 	if(pwd!==pwdC) {
 		alert("Password and Confirm Password Do Not Match");
 		var obj = document.getElementById("signUpPassword");
@@ -33,21 +32,35 @@ function checkRegister(pform) {
 		obj.value = obj.defaultValue;
 		return false;
 	}
+
+	alert('Checking if registered...');
 	var check = $.ajax({
 		type: 'GET',
-		url: root_url + 'registered/' + email,
+		url: root_url + 'registered/' + email + '/' + username,
 		dataType: "json", // data type of response
 		async: false,
+		error: function(jqXHR, textStatus, errorThrown) {
+			alert('Something went wrong\n checkRegistered() error: ' + textStatus + "\nerrorThrown: " + errorThrown);
+		}
 	});
 	check = check.responseJSON;
-	check = check.email_registered;
-	if(check === true) {
+
+	var checkEmail = check.email;
+	var checkUsername = check.username;
+	var checkBool = true;
+	if(checkEmail === true) {
 		alert("Email is already registered");
+		checkBool = false;
 	}
-	else {
+	else if(checkUsername == true) {
+		alert("Username is already registered");
+		checkBool = false;
+	}
+	else if(checkBool == true) {
 		register(pform);
+
 	}
-	return !check;
+	return checkBool;
 }
 
 function register(rform) {
@@ -60,7 +73,7 @@ function register(rform) {
 			alert('User created successfully');
 		},
 		error: function(jqXHR, textStatus, errorThrown){
-			alert('register() error: ' + textStatus + "\nerrorThrown: " + errorThrown);
+			alert('Something went wrong\nregister() error: ' + textStatus + "\nerrorThrown: " + errorThrown);
 		}
 	});
 }
@@ -70,6 +83,7 @@ function regFormToJSON() {
 	return JSON.stringify({
 		"fName": $('#signUpFname').val(),
 		"lName": $('#signUpLname').val(),
+		"username": $('#signUpUsername').val(),
 		"email": $('#signUpEmail').val(),
 		"pass": $('#signUpPassword').val()
 	});
