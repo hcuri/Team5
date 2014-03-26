eea9328047737ffc681cfe1c7426fdfc98a718fd
CaptainMustang8 <nmorris@smu.edu>
2014-03-26 11:11:42 -0500

    Fixed a form

--- user-profile.php
+++ user-profile.php
@@ -20,7 +20,7 @@
 			<input type:"text" name="school/org"/><br/>
 			<h3>School/Organization ID</h3><br/>
 			<input type:"text" name="id"/><br/>
-			<input type:"submit" value="Update Profile"/>
+			<input type:"submit" name="Update Profile"/>
 		</form>
 	</div>
 
