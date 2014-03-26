36f7993962ebbfff0b8cbb5262418cb603491792
CaptainMustang8 <nmorris@smu.edu>
2014-03-26 11:10:27 -0500

    Added a form

--- user-profile.php
+++ user-profile.php
@@ -8,7 +8,22 @@
 	<title>UPresent -- User Profile</title>
 </head>
 <body>
+	<div id="profile">
+		<form name="profile">
+			<h3>First Name:</h3><br/>
+			<input type:"text" name="firstname"/><br/>
+			<h3>Last Name:</h3><br/>
+			<input type:"text" name="lastname"/><br/>
+			<h3>Email:</h3><br/>
+			<input type:"text" name="email"/><br/>
+			<h3>School/Organization</h3><br/>
+			<input type:"text" name="school/org"/><br/>
+			<h3>School/Organization ID</h3><br/>
+			<input type:"text" name="id"/><br/>
+			<input type:"submit" value="Update Profile"/>
+		</form>
+	</div>
 
-	<div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>
+	<!--<div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>-->
 </body>
 </html>
