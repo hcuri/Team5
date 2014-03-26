7e5efd3a029b568fb7fc4bef9de9a43636a98a0d
jkpolitz <jpolitz@smu.edu>
2014-03-25 20:00:10 -0500

    adding first draft of user.php | jquery tabs and tables

---
+++ js/user.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	//$( "#tabs1" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    //$( "#tabs1 li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+	$("#tabs1").tabs();
+	$( "#tabs2" ).tabs();
+});
--- user.php
+++ user.php
@@ -2,16 +2,86 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link href="css/styles_home.css" rel="stylesheet" />
-	<link href="css/styles.css" rel="stylesheet" />
-	<title>UPresent -- User</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/styles_user.css" rel="stylesheet" />
+<link href="css/styles.css" rel="stylesheet" />
+<link href="css/jQuery.css" rel="stylesheet" />
+<script src="js/jQuery.js"></script>
+<script src="js/jquery-1.10.2.js"></script>
+<script src="js/user.js"></script>
+<title>UPresent -- User</title>
 </head>
 <body>
-	<div>Top Bar
-
-
-
-	<div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>
+<div id="content">
+  <div id="tabs1">
+    <ul>
+      <li><a href="#tabs1-1">Current</a></li>
+      <li><a href="#tabs1-2">Ready</a></li>
+    </ul>
+    <div id="tabs1-1">
+      <table>
+        <tr>
+          <td>Next UPresent</td>
+          <td>3/5/14</td>
+          <td><input type="button" value="Present"></td>
+        </tr>
+        <tr>
+          <td>Old UPresent</td>
+          <td>2/8/14</td>
+          <td><input type="button" value="View"></td>
+        </tr>
+      </table>
+    </div>
+    <div id="tabs1-2">Everything inside left box second tab</div>
+  <div id="createNew"><input type="button" value="Create New UPresent"></div>
+  </div>
+  <div id="divider"></div>
+  <div id="tabs2">
+    <ul>
+      <li><a href="#tabs2-1">Upcoming</a></li>
+      <li><a href="#tabs2-2">Past Presentations</a></li>
+    </ul>
+    <div id="tabs2-1">
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Date</th>
+          <th>View</th>
+        <tr>
+          <td>How To Design</td>
+          <td>Christopher Raley</td>
+          <td>1/29/14</td>
+          <td><input type="button" value="View"></td>
+        </tr>
+        <tr>
+          <td>Intro to iOS</td>
+          <td>Hector Curi</td>
+          <td>1/29/14</td>
+          <td><input type="button" value="View"></td>
+        </tr>
+        <tr>
+          <td>My Search Engine</td>
+          <td>Larry Page</td>
+          <td>1/29/14</td>
+          <td><input type="button" value="View"></td>
+        </tr>
+      </table>
+    </div>
+    <div id="tabs2-2"><table>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Date</th>
+          <th>View</th>
+        <tr>
+          <td>Learning From Me</td>
+          <td>Steve Jobs</td>
+          <td>1/15/2012</td>
+          <td><input type="button" value="View"></td>
+        </tr>
+      </table></div>
+  </div>
+</div>
 </body>
 </html>
