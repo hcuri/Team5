d0cd227a1f58d00edaa0dd17ed67041e7ee58ecf
jkpolitz <jpolitz@smu.edu>
2014-03-26 18:32:52 -0500

    editing custom presenter view

--- presenter.php
+++ presenter.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link href="css/styles.css" rel="stylesheet" />
-<link href="css/styles_viewer.css" rel="stylesheet" />
+<link href="css/styles_presenter.css" rel="stylesheet" />
 <link href="css/jQuery.css" rel="stylesheet" />
 <script src="js/jQuery.js"></script>
 <script src="js/viewer.js"></script>
@@ -24,46 +24,21 @@
   <div id="viewer">
     <div id="info">
       <div class="oLine"></div>
-      <h2>Lecture 11</h2>
-      by
-      <h3>Chris Raley</h3>
+      <img id="previous" class="smallSlide" src="img/slide1.jpg"/>
       <div class="oLine"></div>
     </div>
     <div id="slidePane"> <img id="slide" src="img/slide1.jpg"/></div>
     <div id="status">
       <div class="oLine"></div>
-      <h3>Slide</h3>
-      <h2>10/20</h2>
+      <img id="next" class="smallSlide" src="img/slide1.jpg"/>
       <div class="oLine"></div>
     </div>
   </div>
-  <div id="tabs">
-    <ul>
-      <li><a href="#tabs-1">Notes</a></li>
-      <li><a href="#tabs-2">Polls</a></li>
-    </ul>
-    <div id="tabs-1">
-      <div id="notesTab">
-        <textarea rows="5" cols="50" placeholder="Enter Notes Here..."> </textarea>
-      </div>
+  <div id="bottom">
+    <div id="presentNotes">
+      <textarea rows="5" cols="50" placeholder="Enter Notes Here..."> </textarea>
     </div>
-    <div id="tabs-2">
-      <div id="pollsTab">
-        <div id="poll">
-          <p>Please choose a response:</p>
-          <br/>
-          <input type="radio" name="pollResponse" value="A">
-          A<br/>
-          <input type="radio" name="pollResponse" value="B">
-          B<br/>
-          <input type="radio" name="pollResponse" value="C">
-          C<br/>
-          <input type="radio" name="pollResponse" value="D">
-          D<br/>
-        </div>
-        <div id="pollResults"><img id="pollR" src="img/poll.png" /></div>
-      </div>
-    </div>
+    <div id="pollResults"><img id="pollR" src="img/poll.png" /></div>
   </div>
 </div>
 <div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>
