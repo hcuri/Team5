6ac93383e26baa07f0a36bf5e819ec85c57104fd
Hector Curi <hcuri@curi.gateway.pace.com>
2014-03-27 17:56:32 -0500

    Added left side of afterview.php

--- afterview.php
+++ afterview.php
@@ -2,8 +2,63 @@
 <!DOCTYPE HTML>
 <html>
     <head>
+        <link href="css/afterview.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/jQuery.css" rel="stylesheet" />
+        <script src="js/jQuery.js"></script>
+        <script src="js/editor.js"></script>
+        <script src="js/jquery-1.10.2.js"></script>
         <title>UPresent - Afterview</title>
     </head>
     <body>
+        <div id="header">
+            <div id="insideHeader"> <img id="logo" src="img/OfficialMiniLogo.png"/>
+            </div>
+        </div>
+        <div id="content">
+            
+            <!--Title-->
+            <div id="UPresentTitle">
+                <h1>Lecture 11 By Chris Raley</h1>
+            </div>
+            
+            <!--Left Side Content-->
+            <div id="LeftCol">
+                <div id="Slides">
+                    <img src="img/coverview.png" />
+                </div>
+                <div id="NoteSection">
+                    <form id="notes">
+                        <fieldset>
+                            <legend>Notes</legend>
+                            <textarea rows="10" cols="63">       
+    Write helpful notes here. You will be able to view them when you are presenting.
+                            </textarea>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+            
+            <div id="vline"></div>
+            
+            <!--Right Side-->
+            <div id="RightCol">
+                <div id="Chat">
+                    
+                </div>
+                <div id="Options">
+                    
+                </div>
+            </div>
+
+            <!--Footer-->
+            <div id="Footer">
+                <form id="save">
+                    <input type="button" id="home" value="Home" />
+                    <span id="LogoMessage">Created with <img id="bottomlogo" src="img/logoS.png"/></span>
+                </form>
+            </div>
+            
+        </div>
     </body>
 </html>
