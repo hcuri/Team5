52db1a02ad9d43d230c197e8b3b1b265a50a7a13
jkpolitz <jpolitz@smu.edu>
2014-03-27 20:55:49 -0500

    adding back the files that nick deleted....moron

---
+++ invite.php
@@ -0,0 +1,89 @@
+<!--invite.html-->
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/styles.css" rel="stylesheet" />
+<link href="css/styles_invite.css" rel="stylesheet"/>
+<script src="js/main.js"></script>
+<script src="js/jQuery.js"></script>
+<title>UPresent -- Invite</title>
+</head>
+<body>
+<div id="header">
+  <div id="insideHeader"> <img id="logo" src="img/OfficialMiniLogo.png"/></div>
+</div>
+<div id="content">
+  <div id="wrapper">
+    <div id="invite">
+      <input type="text" name="viewersearch" value="Search for users"/>
+      <table id="availableUsers">
+        <tr>
+          <td class="iName">Larry Brown</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Nic Moore</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Nick Russell</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Marcus Kennedy</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Keith Frazier</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Shaun Williams</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Sterling Brown</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+      </table>
+    </div>
+    <div id="divider"></div>
+    <div id="invited">
+      <h2>Invited Users</h2>
+      <table id="invitedUsers">
+        <tr>
+          <td class="iName">John Wayne</td>
+          <td class="iW"><input type="button" name="remove" value="Remove"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Clint Eastwood</td>
+          <td class="iW"><input type="button" name="remove" value="Remove"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Patrick Stewart</td>
+          <td class="iW"><input type="button" name="remove" value="Remove"/></td>
+        </tr>
+        <tr>
+          <td class="iName"></td>
+          <td class="iW"></td>
+        </tr>
+        <tr>
+          <td class="iName"></td>
+          <td class="iW"></td>
+        </tr>
+        <tr>
+          <td class="iName"></td>
+          <td class="iW"></td>
+        </tr>
+        <tr>
+          <td class="iName"></td>
+          <td class="iW"></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
+<div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>
+</body>
+</html>
