90a38ab1e9a631f3e73a5b5b64007ba09d561d36
jkpolitz <jpolitz@smu.edu>
2014-03-27 19:44:24 -0500

    styling invite.php

--- invite.php
+++ invite.php
@@ -2,75 +2,88 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link href="css/styles_home.css" rel="stylesheet" />
-	<link href="css/styles.css" rel="stylesheet" />
-	<link href="css/styles_user.css" rel="stylesheet"/>
-	<link href="css/styles_invite.css" rel="stylesheet"/>
-	<script src="js/main.js"></script>
-  	<script src="js/jQuery.js"></script>
-	<title>UPresent -- Invite</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/styles.css" rel="stylesheet" />
+<link href="css/styles_invite.css" rel="stylesheet"/>
+<script src="js/main.js"></script>
+<script src="js/jQuery.js"></script>
+<title>UPresent -- Invite</title>
 </head>
 <body>
-	<div id="content">
-		<h1>Invite Viewers</h1>
-	<div id="invite">
-		<input type="text" name="viewersearch" value="Search for users"/>
-		<div id="users">
-			<table>
-				<tr>
-					<td>Larry Brown</td>
-					<td><input type="button" name="invite" value="Invite"/></td>
-				</tr>
-				<tr>
-					<td>Nic Moore</td>
-					<td><input type="button" name="invite" value="Invite"/></td>
-				</tr>
-				<tr>
-					<td>Nick Russell</td>
-					<td><input type="button" name="invite" value="Invite"/></td>
-				</tr>
-				<tr>
-					<td>Marcus Kennedy</td>
-					<td><input type="button" name="invite" value="Invite"/></td>
-				</tr>
-				<tr>
-					<td>Keith Frazier</td>
-					<td><input type="button" name="invite" value="Invite"/></td>
-				</tr>
-				<tr>
-					<td>Shaun Williams</td>
-					<td><input type="button" name="invite" value="Invite"/></td>
-				</tr>
-				<tr>
-					<td>Sterling Brown</td>
-					<td><input type="button" name="invite" value="Invite"/></td>
-				</tr>
-			</table>
-		</div>
-	</div>
-	<div id="divider"></div>
-	<div id="invited">
-		<h2>Invited</h2>
-		<table>
-			<tr>
-				<td>John Wayne</td>
-				<td><input type="button" name="remove" value="Remove"/></td>
-			</tr>
-			<tr>
-				<td>Clint Eastwood</td>
-				<td><input type="button" name="remove" value="Remove"/></td>
-			</tr>
-			<tr>
-				<td>Patrick Stewart</td>
-				<td><input type="button" name="remove" value="Remove"/></td>
-			</tr>
-
-
-	</div>
+<div id="header">
+  <div id="insideHeader"> <img id="logo" src="img/OfficialMiniLogo.png"/></div>
 </div>
-
-
+<div id="content">
+  <div id="wrapper">
+    <div id="invite">
+      <input type="text" name="viewersearch" value="Search for users"/>
+      <table id="availableUsers">
+        <tr>
+          <td class="iName">Larry Brown</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Nic Moore</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Nick Russell</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Marcus Kennedy</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Keith Frazier</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Shaun Williams</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Sterling Brown</td>
+          <td class="iW"><input type="button" name="invite" value="Invite"/></td>
+        </tr>
+      </table>
+    </div>
+    <div id="divider"></div>
+    <div id="invited">
+      <h2>Invited Users</h2>
+      <table id="invitedUsers">
+        <tr>
+          <td class="iName">John Wayne</td>
+          <td class="iW"><input type="button" name="remove" value="Remove"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Clint Eastwood</td>
+          <td class="iW"><input type="button" name="remove" value="Remove"/></td>
+        </tr>
+        <tr>
+          <td class="iName">Patrick Stewart</td>
+          <td class="iW"><input type="button" name="remove" value="Remove"/></td>
+        </tr>
+        <tr>
+          <td class="iName"></td>
+          <td class="iW"></td>
+        </tr>
+        <tr>
+          <td class="iName"></td>
+          <td class="iW"></td>
+        </tr>
+        <tr>
+          <td class="iName"></td>
+          <td class="iW"></td>
+        </tr>
+        <tr>
+          <td class="iName"></td>
+          <td class="iW"></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
 <div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>
 </body>
 </html>
