ad776b8df56777a2d48f0860973b324c733b771d
CaptainMustang8 <nmorris@smu.edu>
2014-03-29 15:25:46 -0500

    Added header to some php pages

--- invite.php
+++ invite.php
@@ -10,9 +10,28 @@
 <title>UPresent -- Invite</title>
 </head>
 <body>
-<div id="header">
-  <div id="insideHeader"> <img id="logo" src="img/OfficialMiniLogo.png"/></div>
-</div>
+  <div id="header">
+    <div id="insideHeader"><A HREF="index.php"><img id="logo" src="img/OfficialMiniLogo.png"/></A>
+      <div id="logInPane">
+        <?php
+          $logInForm =  '<form id="login" action="" method="post" onSubmit="return checkLogin(this)">
+                          <input id="logInUsername" type="username" name="username" placeholder="Username" required/>
+                          <input id="logInPassword" type="password" name="password" placeholder="Password" required/>
+                          <input id="logInSubmit" type="submit" name="submit" value="Log In"/>
+                        </form>';
+          if (!empty($_COOKIE['user'])) {
+            $logout = '<DIV ID="logout">Welcome, <span id="logoutUsername">' . $_COOKIE["user"] . '</span>!   <input id="logoutSubmit" type="submit" name="submit" value="Log Out" onClick="logout()" /></DIV>';
+            echo $logout;
+          }
+          else {
+            echo $logInForm;
+          }
+          //echo '<SCRIPT TYPE="text/javascript">alert("' . $_COOKIE["user"] . '");</SCRIPT>';
+        ?>
+        <script>console.log(document.cookie);</script>
+      </div>
+    </div>
+  </div>
 <div id="content">
   <div id="wrapper">
     <div id="invite">
