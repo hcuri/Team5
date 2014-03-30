9740e2bf95294905f1a703590d90b9084de7fdc9
jkpolitz <jpolitz@smu.edu>
2014-03-30 16:42:39 -0500

    user_profile.js updates for connecting with back end

--- js/user_profile.js
+++ js/user_profile.js
@@ -11,14 +11,51 @@ $(document).ready(function() {
 	});
 	info = info.responseJSON;
 	
+	var userN = info.username;
+	$("#userName").html(userN);
+	
 	var fName = info.fName;
 	$("#fName").attr("placeholder", fName);
 	var lName = info.lName;
 	$("#lName").attr("placeholder", lName);
+	
+	var fullName = fName + " " + lName;
+	$("#fullName").html(fullName);
+	
 	var email = info.email;
 	$("#email").attr("placeholder", email);
+	
+	$("#userEmail").html(email);
 	var organization = info.organization;
 	$("#org").attr("placeholder", organization);
 	var orgID = info.schoolID;
 	$("#orgID").attr("placeholder", orgID);
 });
+
+function updateProfile(pForm) {
+	$.ajax({
+		type:"POST",
+		url:root_url + "postUserInfo",
+		data:updateToJSON(),
+		async:false,
+		success: function() {
+			alert("User Info Updated");
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			alert("Something went wrong...error: " + textStatus + "\nerrorThrown: " + errorThrown);
+		}
+	});
+}
+
+function updateToJSON() {
+	return JSON.stringify({
+		"username": $("#userName").html(),
+		"fName": $("#fName").val(),
+		"lName": $("#lName").val(),
+		"email": $("#email").val(),
+		"organization": $("#org").val(),
+		"schoolID": $("#orgID").val(),
+	});
+}
+
+
--- user-profile.php
+++ user-profile.php
@@ -2,23 +2,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link href="css/styles_home.css" rel="stylesheet" />
-	<link href="css/styles.css" rel="stylesheet" />
-	<link href="css/styles_user.css" rel="stylesheet" />
-	<link href="css/styles_user-profile.css" rel="stylesheet" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/styles_home.css" rel="stylesheet" />
+<link href="css/styles.css" rel="stylesheet" />
+<link href="css/styles_user.css" rel="stylesheet" />
+<link href="css/styles_user-profile.css" rel="stylesheet" />
 <link href="css/jQuery.css" rel="stylesheet" />
 <script src="js/jQuery.js"></script>
 <script src="js/jquery-1.10.2.js"></script>
 <script src="js/user_profile.js"></script>
 <script src="js/main.js"></script>
-	<title>UPresent -- User Profile</title>
+<title>UPresent -- User Profile</title>
 </head>
 <body>
-	<div id="header">
-	  <div id="insideHeader"><A HREF="index.php"><img id="logo" src="img/OfficialMiniLogo.png"/></A>
-	    <div id="logInPane">
-	      <?php
+<div id="header">
+  <div id="insideHeader"><A HREF="index.php"><img id="logo" src="img/OfficialMiniLogo.png"/></A>
+    <div id="logInPane">
+      <?php
 	        $logInForm =  '<form id="login" action="" method="post" onSubmit="return checkLogin(this)">
 	                        <input id="logInUsername" type="username" name="username" placeholder="Username" required/>
 	                        <input id="logInPassword" type="password" name="password" placeholder="Password" required/>
@@ -33,41 +33,46 @@
 	        }
 	        //echo '<SCRIPT TYPE="text/javascript">alert("' . $_COOKIE["user"] . '");</SCRIPT>';
 	      ?>
-	      <script>console.log(document.cookie);</script>
-	    </div>
-	  </div>
-	</div>
-	<!--use JavaScript to pull in current values to fill these textfields-->
-	<div id="content">
-		<div id="block">
-			<div id="profile">
-				<form name="profile">
-					<label for="firstname">First Name:</label>
-					<input id="fName" type="text" name="firstname"/><br/>
-					<label for="lastname">Last Name:</label>
-					<input id="lName" type="text" name="lastname"/><br/>
-					<label for="email">Email:</label>
-					<input id="email" type="email" name="email"/><br/>
-					<label for="school/org">School/Organization:</label>
-					<input id="org" type="text" name="school/org"/><br/>
-					<label for="id">School/Organization ID:</label>
-					<input id="orgID" type="text" name="id"/><br/>
-					<input type="submit" name="update" value="Update Profile"/>
-				</form>
-			</div>
-			<div id="divider"></div>
-			<div id="preview">
-				<h3>Profile Preview</h3>
-				<img src="img" alt="Profile Picture">
-				<div id="geninfo">
-					<h2>Larry Brown</h2>
-					<h5>lbrown@smu.edu</h5>
-					<p>Joined on May 12th, 2012</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>
-	<!--<div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>-->
+      <script>console.log(document.cookie);</script> 
+    </div>
+  </div>
+</div>
+<!--use JavaScript to pull in current values to fill these textfields-->
+<div id="content">
+  <div id="block">
+    <div id="profile">
+      <form id="userInfo" name="profile" action="" onSubmit="updateProfile(this)">
+        <label for="firstname">First Name:</label>
+        <input id="fName" type="text" name="firstname"/>
+        <br/>
+        <label for="lastname">Last Name:</label>
+        <input id="lName" type="text" name="lastname"/>
+        <br/>
+        <label for="email">Email:</label>
+        <input id="email" type="email" name="email"/>
+        <br/>
+        <label for="school/org">School/Organization:</label>
+        <input id="org" type="text" name="school/org"/>
+        <br/>
+        <label for="id">School/Organization ID:</label>
+        <input id="orgID" type="text" name="id"/>
+        <br/>
+        <input type="submit" name="update" value="Update Profile"/>
+      </form>
+    </div>
+    <div id="divider"></div>
+    <div id="preview">
+      <h3>Profile Preview</h3>
+      <img src="img" alt="Profile Picture">
+      <div id="geninfo">
+        <h2 id="fullName">Larry Brown</h2>
+        <h3 id="userName">jackjp</h3>
+        <h5 id="userEmail">lbrown@smu.edu</h5>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>
+<!--<div id="footer">UPresent 2014 | About | Terms | Privacy | Contact </div>-->
 </body>
 </html>
