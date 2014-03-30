fcdea2f44ff4d59e86f5c1a8da96f535f4da3dac
jkpolitz <jpolitz@smu.edu>
2014-03-30 15:21:04 -0500

    fixed missed closing tags

--- viewer.php
+++ viewer.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link href="css/styles.css" rel="stylesheet" />
@@ -11,18 +12,30 @@
 </head>
 
 <body>
-<div id="header"><div id="insideHeader"><a href="user.php"/> <img id="logo" src="img/OfficialMiniLogo.png"/></div>
+<div id="header">
+  <div id="insideHeader"><a href="user.php"> <img id="logo" src="img/OfficialMiniLogo.png"/></a></div>
   <!--<div id="navButtons">
     <div id="home"><a href="#home">Home</a></div>
     <div id="account"><a href="#account">Account</a></div>
     <div id="logOff"><a href="#logOff">Log Off</a></div>
-  </div>-->
+  </div>--> 
 </div>
 <div id="content">
   <div id="viewer">
-    <div id="info"><div class="oLine"></div><h2>Lecture 11</h2>by<h3>Chris Raley</h3><div class="oLine"></div></div>
+    <div id="info">
+      <div class="oLine"></div>
+      <h2>Lecture 11</h2>
+      by
+      <h3>Chris Raley</h3>
+      <div class="oLine"></div>
+    </div>
     <div id="slidePane"> <img id="slide" src="img/slide1.jpg"/></div>
-    <div id="status"><div class="oLine"></div><h3>Slide</h3><h2>10/20</h2><div class="oLine"></div></div>
+    <div id="status">
+      <div class="oLine"></div>
+      <h3>Slide</h3>
+      <h2>10/20</h2>
+      <div class="oLine"></div>
+    </div>
   </div>
   <div id="tabs">
     <ul>
@@ -36,11 +49,17 @@
     </div>
     <div id="tabs-2">
       <div id="pollsTab">
-        <div id="poll"><p>Please choose a response:</p><br/>
-          <input type="radio" name="pollResponse" value="A">A<br/>
-          <input type="radio" name="pollResponse" value="B">B<br/>
-          <input type="radio" name="pollResponse" value="C">C<br/>
-          <input type="radio" name="pollResponse" value="D">D<br/>
+        <div id="poll">
+          <p>Please choose a response:</p>
+          <br/>
+          <input type="radio" name="pollResponse" value="A">
+          A<br/>
+          <input type="radio" name="pollResponse" value="B">
+          B<br/>
+          <input type="radio" name="pollResponse" value="C">
+          C<br/>
+          <input type="radio" name="pollResponse" value="D">
+          D<br/>
         </div>
         <div id="pollResults"><img id="pollR" src="img/poll.png" /></div>
       </div>
