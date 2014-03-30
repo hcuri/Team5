42f417cb468269482d9e45e23ea12dc8c3e1eec0
Hector Curi <hcuri@usr158-170.dyn.smu.edu>
2014-03-30 17:01:54 -0500

    Added about.php

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/styles_home.css" rel="stylesheet" />
+<link href="css/styles.css" rel="stylesheet" />
+<script src="js/jQuery.js"></script>
+<script src="js/cookies.js"></script>
+<script src="js/main.js"></script>
+<title>UPresent - About</title>
+</head>
+
+<body>
+<div id="header">
+  <div id="insideHeader"><A HREF="user.php"><img id="logo" src="img/OfficialMiniLogo.png"/></A>
+    <div id="logInPane">
+      <?php
+        $logInForm =  '<form id="login" action="user.php" method="post" onSubmit="return checkLogin(this)">
+                        <input id="logInUsername" type="username" name="username" placeholder="Username" required/>
+                        <input id="logInPassword" type="password" name="password" placeholder="Password" required/>
+                        <input id="logInSubmit" type="submit" name="submit" value="Log In"/>
+                      </form>';
+        if (!empty($_COOKIE['user'])) {
+          $logout = '<DIV ID="logout">Welcome, <span id="logoutUsername">' . $_COOKIE["user"] . '</span>!   <input id="logoutSubmit" type="submit" name="submit" value="Log Out" onClick="logout()" /></DIV>';
+          echo $logout;
+        }
+        else {
+          echo $logInForm;
+        }
+      ?>
+      <script>console.log(document.cookie);</script> 
+    </div>
+  </div>
+</div>
+<div id="content">
+  <div id="terms">
+      <IMG SRC="img/OfficialLogo.png" style="margin-left:250px" />
+    <div id="infoHeader"><br><br>About Us</div>
+    <div id="infoContent"><br>This is where UPresent will describe its history, foundation, etc; for the curious users that want to know more about our company.</div>
+  </div>
+  
+</div>
+<div id="footer">UPresent 2014 | <a href="about.php">About</a> | <a href="terms.php">Terms</a> | <a href="privacy.php">Privacy</a> | <a href="contact.php">Contact</a></div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
