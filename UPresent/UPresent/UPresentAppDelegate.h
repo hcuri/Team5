4ff4696aa0d74d24b9298957ece6b4e93ffbe7d6
Hector Curi <hcuri@usr158-121.dyn.smu.edu>
2014-04-11 16:42:32 -0500

    Added Base iOS App files

---
+++ UPresent/UPresent/UPresentAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UPresentAppDelegate.h
+//  UPresent
+//
+//  Created by Hector Curi on 4/11/14.
+//  Copyright (c) 2014 Team5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UPresentAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
