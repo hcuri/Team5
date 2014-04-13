48cb6fe416b4be5a5a3eaab9763db7ecce9ee248
CaptainMustang8 <nmorris@smu.edu>
2014-04-13 14:20:13 -0500

    Changed afterview some

--- afterview.php
+++ afterview.php
@@ -64,14 +64,14 @@
       <fieldset>
         <legend>Viewers Chat</legend>
         <textarea id="chatHistory" rows="15" cols="63">
-User1: I really liked Raley's UPresent
-                            
-User2: Yeah, he's awesome!
-                            
-User3: I have something important to say to all. "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                            
-User1: That's boring
-                        </textarea>
+          User1: I really liked Raley's UPresent
+                                      
+          User2: Yeah, he's awesome!
+                                      
+          User3: I have something important to say to all. "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                      
+          User1: That's boring
+        </textarea>
         <form id="chatForm">
           <input id="chatInput" type="text" name="chatInput" placeholder="You can comment and chat here" size="50"/>
           <input id="chatSubmit" type="button" value="Send"/>
@@ -79,7 +79,7 @@ User1: That's boring
       </fieldset>
     </div>
     <div id="Options">
-      <input id="download" type="button" value="Download Slides" />
+      <input id="download" type="button" value="Download Slides" /
       <input id="contact" type="button" value="Contact Presenter" />
     </div>
   </div>
