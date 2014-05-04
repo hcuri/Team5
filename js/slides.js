72054333ff7fd70a6db00d9f8fd78d1b364967f5
jkpolitz <jpolitz@smu.edu>
2014-05-04 14:34:58 -0500

    fixed poll issue for viewer.php

--- js/slides.js
+++ js/slides.js
@@ -287,7 +287,8 @@ var getPollResults = function() {
 	result = result.responseJSON;
 	
 	for(var i = 0; i < numQ; i++) {
-		data.setValue(i, 1, result[i].option_results);
+		var resultIndex = result[i].option_num;
+		data.setValue(letters.indexOf(resultIndex), 1, result[i].option_results);
 	}
 	
 	chart = new google.visualization.ColumnChart(document.getElementById('bInfoGraph'));
